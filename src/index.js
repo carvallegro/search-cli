e995551485bd237565a5a2ef959fcb53921a6be9
Carvallegro <carvallegro@users.noreply.github.com>
2018-11-14 23:16:15 +1100

    Feature/project setup (#1)
    * Kickstarted the project with a few dependencies and minimalistic code
    
    * removes node_modules
    
    * gitignore and cleanup
    
    * added prettier
    
    * Added first test
    
    * Added TravisCI config
    
    * Added TravisCI config
    
    * fix to make Travis CI pass
    
    * Added npm i in travis
    
    * fix test command in Travis config
    
    * Added a few more repo and fixed test for CI
    
    * Added few TODOs

---
+++ src/index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+var program = require('commander')
+
+var package = require('../package.json')
+
+program.version(package.version)
+
+program.on('--help', function() {
+  console.log('')
+  console.log('About:')
+  console.log(
+    '  This application allows you to search through a few JSON files.'
+  )
+  console.log('')
+})
+
+program.parse(process.argv)
+
+console.log('Hello World')
+
+// TODO:
+// - core search engine
+// - params parsing (query generation)
+// - display templates
