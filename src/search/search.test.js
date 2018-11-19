f8d8e48e6c4022bb3fc0dd9b35061bae5b1bb991
Carvallegro <carvallegro@users.noreply.github.com>
2018-11-19 22:02:27 +1100

    fixed test

--- src/search/search.test.js
+++ src/search/search.test.js
@@ -100,37 +100,16 @@ describe('the search functions', () => {
   })
 
   describe('Search user by id', () => {
-    const expected = {
-      _id: 7,
-      url: 'http://initech.zendesk.com/api/v2/users/7.json',
-      external_id: 'bce94e82-b4f4-438f-bc0b-2440e8265705',
-      name: 'Lou Schmidt',
-      alias: 'Miss Shannon',
-      created_at: '2016-05-07T08:43:52 -10:00',
-      active: false,
-      verified: false,
-      shared: false,
-      locale: 'en-AU',
-      timezone: 'Central African Republic',
-      last_login_at: '2016-02-25T12:26:31 -11:00',
-      email: 'shannonschmidt@flotonic.com',
-      phone: '9094-083-167',
-      signature: 'Don\'t Worry Be Happy!',
-      organization_id: 104,
-      tags: ['Cawood', 'Disautel', 'Boling', 'Southview'],
-      suspended: true,
-      role: 'admin'
-    }
     it('should return the expected user with string id', () => {
       const actual = findUserById('7')
 
-      expect(actual).to.deep.equal(expected)
+      expect(actual).to.deep.equal(expectedUser)
     })
 
     it('should return the expected user with integer id', () => {
       const actual = findUserById(7)
 
-      expect(actual).to.deep.equal(expected)
+      expect(actual).to.deep.equal(expectedUser)
     })
 
     it('should return undefined when no id', () => {
