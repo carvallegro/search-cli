42a27ea0061057f96ca681a6ddc9f3d1936ae5fc
Carvallegro <carvallegro@users.noreply.github.com>
2018-11-19 21:39:52 +1100

    Add find by id data

--- src/search/search.test.js
+++ src/search/search.test.js
@@ -1,6 +1,6 @@
 const expect = require('chai').expect
 
-const { userSearch, ticketSearch, organizationSearch } = require('./index')
+const { userSearch, ticketSearch, organizationSearch, searchUserById, searchOrganizationById } = require('./index')
 
 describe('the search functions', () => {
   describe('the user search', () => {
@@ -25,7 +25,7 @@ describe('the search functions', () => {
         last_login_at: '2016-02-25T12:26:31 -11:00',
         email: 'shannonschmidt@flotonic.com',
         phone: '9094-083-167',
-        signature: "Don't Worry Be Happy!",
+        signature: 'Don\'t Worry Be Happy!',
         organization_id: 104,
         tags: ['Cawood', 'Disautel', 'Boling', 'Southview'],
         suspended: true,
@@ -95,6 +95,67 @@ describe('the search functions', () => {
 
       expect(actual).to.have.length(1)
       expect(actual[0]).to.deep.equal(expected)
+    })
+  })
+
+  describe('Search user by id', () => {
+    const expected = {
+      _id: 7,
+      url: 'http://initech.zendesk.com/api/v2/users/7.json',
+      external_id: 'bce94e82-b4f4-438f-bc0b-2440e8265705',
+      name: 'Lou Schmidt',
+      alias: 'Miss Shannon',
+      created_at: '2016-05-07T08:43:52 -10:00',
+      active: false,
+      verified: false,
+      shared: false,
+      locale: 'en-AU',
+      timezone: 'Central African Republic',
+      last_login_at: '2016-02-25T12:26:31 -11:00',
+      email: 'shannonschmidt@flotonic.com',
+      phone: '9094-083-167',
+      signature: 'Don\'t Worry Be Happy!',
+      organization_id: 104,
+      tags: ['Cawood', 'Disautel', 'Boling', 'Southview'],
+      suspended: true,
+      role: 'admin'
+    }
+    it('should return the expected user with string id', () => {
+      const actual = searchUserById('7')
+
+      expect(actual).to.deep.equal(expected)
+    })
+
+    it('should return the expected user with integer id', () => {
+      const actual = searchUserById(7)
+
+      expect(actual).to.deep.equal(expected)
+    })
+  })
+
+  describe('Search organization by id', () => {
+    const expected = {
+      _id: 101,
+      url: 'http://initech.zendesk.com/api/v2/organizations/101.json',
+      external_id: '9270ed79-35eb-4a38-a46f-35725197ea8d',
+      name: 'Enthaze',
+      domain_names: ['kage.com', 'ecratic.com', 'endipin.com', 'zentix.com'],
+      created_at: '2016-05-21T11:10:28 -10:00',
+      details: 'MegaCorp',
+      shared_tickets: false,
+      tags: ['Fulton', 'West', 'Rodriguez', 'Farley']
+    }
+
+    it('should return the expected organization with string id', () => {
+      const actual = searchOrganizationById('101')
+
+      expect(actual).to.deep.equal(expected)
+    })
+
+    it('should return the expected organization with integer id', () => {
+      const actual = searchOrganizationById(101)
+
+      expect(actual).to.deep.equal(expected)
     })
   })
 })
